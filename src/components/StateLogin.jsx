50b7a3566b63d4dd9cad2b1b6e57e4b293cca0d1
mario <mario_reyes505@hotmail.com>
2026-06-05 22:48:36 -0500

    added use of validation.js to StateLogin.jsx

--- src/components/StateLogin.jsx
+++ src/components/StateLogin.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Input from "./Input.jsx";
+import { isEmail, isNotEmpty, hasMinLength } from "../util/validation.js";
 
 export default function Login() {
   const [enteredValues, setEnteredValues] = useState({
@@ -12,9 +13,12 @@ export default function Login() {
     password: false,
   });
 
-  const eamilIsInvalid = didEdit.email && !enteredValues.email.includes("@");
+  const eamilIsInvalid =
+    didEdit.email &&
+    !isEmail(enteredValues.email) &&
+    !isNotEmpty(enteredValues.email);
   const passwordIsInvalid =
-    didEdit.password && enteredValues.password.trim().length < 6;
+    didEdit.password && !hasMinLength(enteredValues.password, 6);
 
   const handleSubmit = (event) => {
     event.preventDefault();
